97d921fd65e10afde32406dcee6dde8aa150e97c
CarlosT25-png <cartorresva@gmail.com>
2023-05-04 10:03:37 -0600

    fix commit

--- 3d-portafolio/src/components/Experience.tsx
+++ 3d-portafolio/src/components/Experience.tsx
@@ -23,6 +23,7 @@ const Experience = () => {
   return (
     <>
       <Perf position="top-left" />
+      
       <color args={["#241a1a"]} attach="background" />
 
       <OrbitControls makeDefault enabled={true} />
