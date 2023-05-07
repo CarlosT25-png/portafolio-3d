351617d7e3758133d3a8586ecdc5a47b1536bb09
CarlosT25-png <cartorresva@gmail.com>
2023-05-07 13:18:03 -0600

    loader

--- 3d-portafolio/src/components/IndexExperience.tsx
+++ 3d-portafolio/src/components/IndexExperience.tsx
@@ -1,30 +1,35 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
+import { Loader } from "@react-three/drei";
 
 const IndexExperience = () => {
-
-  const [ width, setWidth ] = useState('')
-  const [ height, setHeight ] = useState('')
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
 
   useEffect(() => {
-    setWidth(`${window.innerWidth}px`)
-    setHeight(`${window.innerHeight}px`)
-  }, [])
+    setWidth(`${window.innerWidth}px`);
+    setHeight(`${window.innerHeight}px`);
+  }, []);
 
   return (
-    <Canvas
-      dpr={[ 1, 2 ]}
-      camera={{
-        fov: 35,
-        near: 0.1,
-        far: 2000,
-        position: [-4.8, -0.2, 5.2],
-      }}
-      style={{ width: width, height: height }}
-    >
-      <Experience />
-    </Canvas>
+    <>
+      <Suspense fallback={ null }>
+        <Canvas
+          dpr={[1, 2]}
+          camera={{
+            fov: 35,
+            near: 0.1,
+            far: 2000,
+            position: [-4.8, -0.2, 5.2],
+          }}
+          style={{ width: width, height: height }}
+        >
+          <Experience />
+        </Canvas>
+      </Suspense>
+      <Loader />
+    </>
   );
 };
 
