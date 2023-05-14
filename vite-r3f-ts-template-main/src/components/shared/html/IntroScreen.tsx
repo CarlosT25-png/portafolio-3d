b9258beeb07381a0bd2a84a496c3bfc652fbd1e3
CarlosT25-png <cartorresva@gmail.com>
2023-05-14 12:46:22 -0600

    lava lamp added and bloom effect

---
+++ vite-r3f-ts-template-main/src/components/shared/html/IntroScreen.tsx
@@ -0,0 +1,9 @@
+const IntroScreen = () => {
+  return (
+    <div style={{ height: `${window.innerHeight}px`, width: `${window.innerWidth}px` }}>
+      
+    </div>
+  );
+}
+
+export default IntroScreen;
