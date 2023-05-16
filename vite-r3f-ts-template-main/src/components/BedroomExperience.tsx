1569ae5f6494e8a0133f9cbac3535b5e0570f270
CarlosT25-png <cartorresva@gmail.com>
2023-05-16 00:58:04 -0600

    animation added, but there is a bug

--- vite-r3f-ts-template-main/src/components/BedroomExperience.tsx
+++ vite-r3f-ts-template-main/src/components/BedroomExperience.tsx
@@ -10,7 +10,7 @@ const BedroomExperience = () => {
 
       <color args={["#241a1a"]} attach="background" />
 
-      <OrbitControls makeDefault enabled={true} />
+      {/* <OrbitControls makeDefault enabled={true} /> */}
 
       <Center>
         <BasicWorld />
--- vite-r3f-ts-template-main/src/components/bedroomExperience/world/Structure.tsx
+++ vite-r3f-ts-template-main/src/components/bedroomExperience/world/Structure.tsx
@@ -1,8 +1,9 @@
+import { useLayoutEffect, useRef, useState } from 'react'
+import { useThree } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
-import { useFrame, useThree } from '@react-three/fiber'
-import { useControls } from 'leva'
-import { useRef, useState } from 'react'
 import * as THREE from 'three'
+import { useControls } from 'leva'
+import gsap from 'gsap'
 
 interface BedroomInterface {
   nodes: {
@@ -18,9 +19,9 @@ interface BedroomInterface {
 }
 
 const Structure = () => {
-  const [ isClicked, setIsClicked ] = useState(false);
+  const [ isEnterPlaying , setIsEnterPlaying ] = useState(false)
   const pcRef = useRef<THREE.Group>(null!)
-  const { camera } = useThree()
+  const { camera, controls } = useThree()
   const model = useGLTF(
     '/models/bedroomScene/bedroom-draco.glb'
   ) as unknown as BedroomInterface
@@ -29,33 +30,79 @@ const Structure = () => {
 
   const { rotationObj, positionObj } = useControls('cameraPos', {
     rotationObj: {
-      value: [-0.31, -0.64, -0.19],
+      value: [0.038442, -0.7450511, 0.026071],
       step: 0.01,
       joystick: 'invertY',
     },
     positionObj: {
-      value: [-4.24, 0.26, 4.76], //value: [-4.24, 0.26, 4.76],
+      value: [-4.8, -0.2, 5.2], //value: [-4.24, 0.26, 4.76],
       step: 0.01,
       joystick: 'invertY',
     },
   })
 
-  useFrame(( state ) => {
-    if(isClicked) {
-      state.camera.lookAt(pcRef.current?.position)
-      state.camera.position.lerp( new THREE.Vector3(1,1,1), 0.1 )
-      state.camera.updateProjectionMatrix();
-    }
-  })
+  useLayoutEffect(() => {
+    camera.position.set(-2.43, 0.72, 2.55)
+    camera.rotation.set(-0.32, -0.74, -0.22)
+  }, [])
 
-  const clickHandler = () => {
-    console.log('click')
-    // camera.position.set(model.nodes.monitor001.position.x, model.nodes.monitor001.position.y, model.nodes.monitor001.position.z)
+  const mouseEnterAnimation = () => {
+    setIsEnterPlaying(true)
+    camera.lookAt(model.nodes.monitor001.position)
+    // camera.position.set( 0.59, -0.24, -0.4125 ) //...positionObj
+    gsap.to(camera.position, {
+      x: 0.59,
+      y: -0.24,
+      z: -0.4125,
+      duration: 1.5,
+    })
+    gsap.to(camera.rotation, {
+      x: -1.13,
+      y: -1.51,
+      z: -1.13,
+      duration: 1.5,
+    })
+    // camera.rotation.set( -1.13, -1.51, -1.13) //...rotationObj
+
+    setTimeout(() => {
+      setIsEnterPlaying(false)
+    }, 1500)
+  
   }
 
-  // camera.lookAt(model.nodes.monitor001.position)
-  // camera.position.set(...positionObj)
-  // camera.rotation.set(...rotationObj)
+  const mouseLeaveAnimation = () => {
+    camera.lookAt(new THREE.Vector3())
+    // camera.position.set( -2.43, 0.72, 2.55 ) //...positionObj
+    // camera.rotation.set( -0.32, -0.74, -0.22 ) //...rotationObj
+    gsap.to(camera.position, {
+      x: -2.43,
+      y: 0.72,
+      z: 2.55,
+      duration: 1.5,
+    })
+    gsap.to(camera.rotation, {
+      x: -0.32,
+      y: -0.74,
+      z: -0.22,
+      duration: 1.5,
+    })
+
+    setTimeout(() => {
+      setIsEnterPlaying(false)
+    }, 1500)
+  }
+
+  const onMouseEnter = () => {
+    if(!isEnterPlaying){
+      mouseEnterAnimation()
+    }
+  }
+
+  const onMouseLeave = () => {
+    if(!isEnterPlaying){
+      mouseLeaveAnimation()
+    }
+  }
 
   return (
     <group>
@@ -63,10 +110,7 @@ const Structure = () => {
       <primitive object={model.nodes.scene001} />
       <primitive object={model.nodes.library} />
       <primitive object={model.nodes.periferics} />
-      <group ref={ pcRef } onClick={() => {
-        console.log('first')
-        setIsClicked(!isClicked)
-      }}>
+      <group ref={pcRef} onPointerEnter={onMouseEnter} onPointerLeave={onMouseLeave}>
         <primitive object={model.nodes.monitor001} />
       </group>
     </group>
