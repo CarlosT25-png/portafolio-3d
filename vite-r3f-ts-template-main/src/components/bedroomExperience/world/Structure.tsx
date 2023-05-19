f0743bd6abdf26a999506a082ac027b0c5ec6592
CarlosT25-png <cartorresva@gmail.com>
2023-05-19 14:03:28 -0600

    Iframe added

--- vite-r3f-ts-template-main/src/components/bedroomExperience/world/Structure.tsx
+++ vite-r3f-ts-template-main/src/components/bedroomExperience/world/Structure.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { useThree } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { Html, useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 import { useControls } from 'leva'
 import gsap from 'gsap'
+import { EffectComposer } from '@react-three/postprocessing'
 
 interface BedroomInterface {
   nodes: {
@@ -20,6 +21,7 @@ interface BedroomInterface {
 
 const Structure = () => {
   const [isEnterPlaying, setIsEnterPlaying] = useState(false)
+  const [showIframe, setShowIframe] = useState(false)
   const pcRef = useRef<THREE.Group>(null!)
   const screenRef = useRef<THREE.Mesh>(null!)
   const { camera, size } = useThree()
@@ -29,18 +31,37 @@ const Structure = () => {
 
   // Debug
 
-  const { rotationObj, positionObj } = useControls('cameraPos', {
-    rotationObj: {
-      value: [0, -Math.PI * 0.5, 0],
-      step: 0.01,
-      joystick: 'invertY',
-    },
-    positionObj: {
-      value: [0.89, 0.46, 0.4275], //value: [-4.24, 0.26, 4.76],
-      step: 0.01,
-      joystick: 'invertY',
-    },
-  })
+  // const { rotationObj, positionObj, distanceFactor } = useControls('html', {
+  //   rotationObj: {
+  //     value: [-1.56, -1.28, -1.56],
+  //     step: 0.01,
+  //     joystick: 'invertY',
+  //   },
+  //   positionObj: {
+  //     value: [1.16, 0.44, 0.42], //value: [-4.24, 0.26, 4.76],
+  //     step: 0.001,
+  //     joystick: 'invertY',
+  //   },
+  //   distanceFactor: {
+  //     value: 0.105, //value: [-4.24, 0.26, 4.76],
+  //     step: 0.0001,
+  //     joystick: 'invertY',
+  //   },
+  // })
+
+  // const { rotationBack, postionBack, color } = useControls('screenPlane', {
+  //   rotationBack: {
+  //     value: [-1.56, -1.23, -1.56],
+  //     step: 0.01,
+  //     joystick: 'invertY',
+  //   },
+  //   postionBack: {
+  //     value: [0.89, 0.46, 0.4275], //value: [-4.24, 0.26, 4.76],
+  //     step: 0.001,
+  //     joystick: 'invertY',
+  //   },
+  //   color: "#ffffff"
+  // })
 
   useLayoutEffect(() => {
     camera.position.set(-2.43, 0.72, 2.55)
@@ -48,6 +69,7 @@ const Structure = () => {
   }, [])
 
   const showIframeHandler = () => {
+    setShowIframe(true)
     // Get the position of the mesh in 3D space
     const { x, y, z } = screenRef.current.position
 
@@ -63,8 +85,7 @@ const Structure = () => {
     console.log(screenPosition)
     // Convert the projected coordinates to pixels
     const pixelX = (screenPosition.x * 0.5 + 0.5) * size.width
-    const pixelY =
-      (1 - (screenPosition.y * 0.5 + 0.5)) * size.height
+    const pixelY = (1 - (screenPosition.y * 0.5 + 0.5)) * size.height
 
     // Use the pixel coordinates as needed
     console.log('Mesh position in pixels:', pixelX, pixelY)
@@ -95,6 +116,7 @@ const Structure = () => {
   const mouseLeaveAnimation = () => {
     // camera.position.set( -2.43, 0.72, 2.55 ) //...positionObj
     // camera.rotation.set( -0.32, -0.74, -0.22 ) //...rotationObj
+    setShowIframe(false)
     gsap.to(camera.position, {
       x: -2.43,
       y: 0.72,
@@ -140,14 +162,32 @@ const Structure = () => {
         <primitive object={model.nodes.monitor001} />
       </group>
       {/* This will act as the real screen */}
-      <mesh
-        ref={screenRef}
-        rotation={[-1.56, -1.23, -1.56]}
-        position={[0.89, 0.46, 0.4275]}
-      >
-        <planeGeometry args={[0.12, 0.092]} />
-        <meshBasicMaterial color={'yellow'} />
-      </mesh>
+      <scene>
+        <mesh
+          ref={screenRef}
+          rotation={[-1.51, -1.22, -1.51]}
+          position={[0.89, 0.46, 0.43]}
+        >
+          <planeGeometry args={[0.118, 0.0915]} />
+          <meshBasicMaterial color={'#5c5c5c'} />
+        </mesh>
+        {showIframe && (
+          <Html
+            transform
+            distanceFactor={0.11}
+            rotation={[-1.56, -1.29, -1.56]}
+            position={[1.16, 0.44, 0.42]}
+          >
+            <iframe
+              src='https://carlostorres.dev'
+              style={{ width: '1000px', height: '750px', border: 'none' }}
+            />
+          </Html>
+        )}
+      </scene>
+
+      {/* Effect Composer */}
+      {/* <EffectComposer scene={}></EffectComposer> */}
     </group>
   )
 }
