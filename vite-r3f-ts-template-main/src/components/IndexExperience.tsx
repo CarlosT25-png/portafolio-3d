c10d82aca7c4f6c5d2a6a7c3368f3b95c22bf627
CarlosT25-png <cartorresva@gmail.com>
2023-05-20 09:46:01 -0600

    hook to handle resize

--- vite-r3f-ts-template-main/src/components/IndexExperience.tsx
+++ vite-r3f-ts-template-main/src/components/IndexExperience.tsx
@@ -1,20 +1,33 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import TimeMachineExperience from './TimeMachineExperience'
-import { Suspense, useEffect, useMemo, useState } from 'react'
+import { Suspense, useEffect, useLayoutEffect, useMemo, useState } from 'react'
 import { Loader, Preload } from '@react-three/drei'
 import BedroomExperience from './BedroomExperience'
 import { ACESFilmicToneMapping, CineonToneMapping, sRGBEncoding } from 'three'
 import IntroScreen from './shared/html/IntroScreen'
+function useWindowSize() {
+  const [sizes, setSizes] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSizes([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return sizes;
+}
 
 const IndexExperience = () => {
+  const [widthR, heightR] = useWindowSize();
   const [isStarted, setIsStarted] = useState(false)
-  const [width, setWidth] = useState('')
-  const [height, setHeight] = useState('')
+  // const [width, setWidth] = useState('')
+  // const [height, setHeight] = useState('')
 
-  useEffect(() => {
-    setWidth(`${window.innerWidth}px`)
-    setHeight(`${window.innerHeight}px`)
-  }, [])
+  // useEffect(() => {
+  //   setWidth(`${window.innerWidth}px`)
+  //   setHeight(`${window.innerHeight}px`)
+  // }, [])
 
   return (
     <>
@@ -35,10 +48,10 @@ const IndexExperience = () => {
               far: 2000,
               position: [-4.8, -0.2, 5.2],
             }}
-            style={{ width: width, height: height }}
+            style={{ width: widthR, height: heightR }}
             shadows
           >
-            {/* <TimeMachineExperience /> */}
+            <TimeMachineExperience />
             {/* <BedroomExperience /> */}
             <Preload all  />
           </Canvas>
--- vite-r3f-ts-template-main/src/main.tsx
+++ vite-r3f-ts-template-main/src/main.tsx
@@ -1,30 +1,31 @@
-import { Canvas } from '@react-three/fiber'
 import { Leva } from 'leva'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ACESFilmicToneMapping, sRGBEncoding } from 'three'
-import { Scene } from './Scene'
 import './styles/main.css'
 import IndexExperience from './components/IndexExperience'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function Main() {
   return (
-    <div className='main'>
-      <Leva
-        collapsed={false}
-        oneLineLabels={false}
-        flat={true}
-        theme={{
-          sizes: {
-            titleBarHeight: '28px',
-          },
-          fontSizes: {
-            root: '10px',
-          },
-        }}
-      />
-      <IndexExperience />
-    </div>
+    <Provider store={store}>
+      <div className='main'>
+        <Leva
+          collapsed={false}
+          oneLineLabels={false}
+          flat={true}
+          theme={{
+            sizes: {
+              titleBarHeight: '28px',
+            },
+            fontSizes: {
+              root: '10px',
+            },
+          }}
+        />
+        <IndexExperience />
+      </div>
+    </Provider>
   )
 }
 
