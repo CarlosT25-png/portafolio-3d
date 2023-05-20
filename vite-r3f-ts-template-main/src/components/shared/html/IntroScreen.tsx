d38e66bd1f151a9c955655f7793717a108ef22e5
CarlosT25-png <cartorresva@gmail.com>
2023-05-20 09:34:28 -0600

    Intro screen done

--- vite-r3f-ts-template-main/src/components/shared/html/IntroScreen.tsx
+++ vite-r3f-ts-template-main/src/components/shared/html/IntroScreen.tsx
@@ -1,25 +1,33 @@
-import { Dispatch, SetStateAction } from 'react';
-import styles from './IntroScreen.module.css';
+import { Dispatch, SetStateAction } from 'react'
+import styles from './IntroScreen.module.css'
 
 interface Props {
   onStart: Dispatch<SetStateAction<boolean>>
 }
 
-const IntroScreen = ( { onStart }: Props ) => {
-
+const IntroScreen = ({ onStart }: Props) => {
   const clickHandler = () => {
     onStart(true)
   }
 
   return (
-    <div style={{ height: `${window.innerHeight}px`, width: `${window.innerWidth}px` }} className={styles.main}>
+    <div
+      style={{ height: `${window.innerHeight}px`, width: `${window.innerWidth}px` }}
+      className={styles.main}
+    >
       <div className={styles['message-container']}>
-        <h2>Welcome to my portafolio</h2>
-        <p>I hope you enjoy it</p>
-        <a onClick={clickHandler} className={`${styles['btn']} ${styles['btn-1']}`}>Start</a>
+        <h2 className={styles['title']}>👋 Welcome to My Portfolio</h2>
+        <p className={styles['text']}>
+          Hey there! Get ready to explore an extraordinary world of web development! From
+          captivating designs to seamless user experiences, my portfolio is a showcase of
+          my passion for frontend development. Enjoy the ride!
+        </p>
+        <a onClick={clickHandler} className={`${styles['btn']} ${styles['btn-1']}`}>
+          Let's Dive In! 🚀
+        </a>
       </div>
     </div>
-  );
+  )
 }
 
-export default IntroScreen;
+export default IntroScreen
