d1ddd2ffae72ade42a8ff76b8ef306c3e088dbf2
CarlosT25-png <cartorresva@gmail.com>
2023-05-26 00:29:54 -0600

    continue snake game

--- snake-game/src/game/Game.tsx
+++ snake-game/src/game/Game.tsx
@@ -1,9 +1,10 @@
 import styles from './Game.module.css'
+import GameBoard from './GameBoard';
 
 const Game = () => {
   return (
     <div style={{ width: `${window.innerWidth}px`, height: `${window.innerHeight}px`}} className={styles['main-container']}>
-      
+      <GameBoard width={800} height={700} />
     </div>
   );
 }
--- snake-game/src/game/GameBoard.tsx
+++ snake-game/src/game/GameBoard.tsx
@@ -1,12 +1,50 @@
+import { ReactNode, Ref, useEffect, useRef, useState } from 'react';
+import styles from './GameBoard.module.css';
+
 interface Props {
   height: number,
   width: number
 }
 
 const GameBoard = ( { width, height }: Props ) => {
-  return (
-    <div style={{ width, height}} className="">
+  const columns = Math.trunc(width / 20 )
+  const rows = Math.trunc(height / 20)
 
+  const [gameBoardCells, _] = useState<Array<Array<ReactNode>>>(new Array<Array<ReactNode>>(rows))
+  const [gameBoardCellsRef, setGameBoardCellsRef] = useState<Array<Array<Ref<HTMLDivElement>>>>(new Array<Array<Ref<HTMLDivElement>>>(rows))
+  
+  for(let i = 0; i < gameBoardCells.length; i++){
+    gameBoardCells[i] = new Array<ReactNode>(columns)
+  }
+
+  for (let i = 0; i < gameBoardCells.length; i++) {
+    for(let j = 0; j < columns ; j++ ){
+      const divRef = useRef<HTMLDivElement>(null!)
+      gameBoardCells[i][j] = <div ref={divRef} key={`${i}+${j}`} className={styles['grid-item']}></div>
+      gameBoardCellsRef[i][j] = divRef
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if( gameBoardCellsRef ) {
+
+        if(gameBoardCellsRef[rows/2][columns/2] !== null ){
+          gameBoardCellsRef[rows/2].find()
+        }
+        
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+  return (
+    <div style={{ width, height, gridTemplateColumns: `repeat(${columns}, 20px)`, gridTemplateRows: `repeat(${rows}, 20px)`}} className={styles['game-board']}>
+      {gameBoardCells.map(row => {
+        return row.map(column => column)
+      })}
     </div>
   );
 }
