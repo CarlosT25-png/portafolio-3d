fa8e1bc56de26cd76212863b5774a56eaa5497eb
CarlosT25-png <cartorresva@gmail.com>
2023-05-26 18:49:05 -0600

    game logic is done

--- snake-game/src/game/GameBoard.tsx
+++ snake-game/src/game/GameBoard.tsx
@@ -1,52 +1,165 @@
-import { ReactNode, Ref, useEffect, useRef, useState } from 'react';
-import styles from './GameBoard.module.css';
+import { useEffect, useRef, useState } from "react";
+import styles from "./GameBoard.module.css";
+import AppleLogo from "/apple.png";
+import useInterval from "./useInterval";
 
-interface Props {
-  height: number,
-  width: number
-}
+// interface Props {
+//   height: number,
+//   width: number
+// }
 
-const GameBoard = ( { width, height }: Props ) => {
-  const columns = Math.trunc(width / 20 )
-  const rows = Math.trunc(height / 20)
+const canvasX = 1000;
+const canvasY = 1000;
+const initialSnake = [
+  [4, 10],
+  [4, 10],
+];
+const initialApple = [14, 10];
+const scale = 50;
+const timeDelay = 100;
 
-  const [gameBoardCells, _] = useState<Array<Array<ReactNode>>>(new Array<Array<ReactNode>>(rows))
-  const [gameBoardCellsRef, setGameBoardCellsRef] = useState<Array<Array<Ref<HTMLDivElement>>>>(new Array<Array<Ref<HTMLDivElement>>>(rows))
-  
-  for(let i = 0; i < gameBoardCells.length; i++){
-    gameBoardCells[i] = new Array<ReactNode>(columns)
+const GameBoard = () => {
+  // { width, height }: Props
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [direction, setDirection] = useState([0, -1]);
+  const [delay, setDelay] = useState<number | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  useInterval(() => runGame(), delay);
+
+  useEffect(() => {
+    let fruit = document.getElementById("fruit") as HTMLCanvasElement;
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        ctx.fillStyle = "#a3d001";
+        snake.forEach(([x, y]) => ctx.fillRect(x, y, 1, 1));
+        ctx.drawImage(fruit, apple[0], apple[1], 1, 1);
+      }
+    }
+  }, [snake, apple, gameOver]);
+
+  function play() {
+    setSnake(initialSnake);
+    setApple(initialApple);
+    setDirection([1, 0]);
+    setDelay(timeDelay);
+    setScore(0);
+    setGameOver(false);
   }
 
-  for (let i = 0; i < gameBoardCells.length; i++) {
-    for(let j = 0; j < columns ; j++ ){
-      const divRef = useRef<HTMLDivElement>(null!)
-      gameBoardCells[i][j] = <div ref={divRef} key={`${i}+${j}`} className={styles['grid-item']}></div>
-      gameBoardCellsRef[i][j] = divRef
+  function checkCollision(head: number[]) {
+    for (let i = 0; i < head.length; i++) {
+      if (head[i] < 0 || head[i] * scale >= canvasX) return true;
+    }
+    for (const s of snake) {
+      if (head[0] === s[0] && head[1] === s[1]) return true;
+    }
+    return false;
+  }
+
+  function appleAte(newSnake: number[][]) {
+    let coord = apple.map(() => Math.floor((Math.random() * canvasX) / scale));
+    if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+      let newApple = coord;
+      setScore(score + 1);
+      setApple(newApple);
+      return true;
+    }
+    return false;
+  }
+
+  function handleSetScore() {
+    if (score > Number(localStorage.getItem("snakeScore"))) {
+      localStorage.setItem("snakeScore", JSON.stringify(score));
+    }
+  }
+
+  function runGame() {
+    const newSnake = [...snake];
+    const newSnakeHead = [
+      newSnake[0][0] + direction[0],
+      newSnake[0][1] + direction[1],
+    ];
+    newSnake.unshift(newSnakeHead);
+    if (checkCollision(newSnakeHead)) {
+      setDelay(null);
+      setGameOver(true);
+      handleSetScore();
+    }
+    if (!appleAte(newSnake)) {
+      newSnake.pop();
+    }
+    setSnake(newSnake);
+  }
+
+  function changeDirection(e: KeyboardEvent) {
+    switch (e.key) {
+      case "ArrowLeft":
+        setDirection([-1, 0]);
+        break;
+      case "ArrowUp":
+        setDirection([0, -1]);
+        break;
+      case "ArrowRight":
+        setDirection([1, 0]);
+        break;
+      case "ArrowDown":
+        setDirection([0, 1]);
+        break;
     }
   }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if( gameBoardCellsRef ) {
-
-        if(gameBoardCellsRef[rows/2][columns/2] !== null ){
-          gameBoardCellsRef[rows/2].find()
-        }
-        
+    const handleKeyDown = (ev: KeyboardEvent) => {
+      if (ev.code === "Space") {
+        play();
+      } else {
+        changeDirection(ev);
       }
-    }, 1000);
+    };
 
-    return () => clearInterval(interval);
+    document.body.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.body.removeEventListener("keydown", handleKeyDown);
+    };
   }, []);
 
-
   return (
-    <div style={{ width, height, gridTemplateColumns: `repeat(${columns}, 20px)`, gridTemplateRows: `repeat(${rows}, 20px)`}} className={styles['game-board']}>
-      {gameBoardCells.map(row => {
-        return row.map(column => column)
-      })}
+    <div
+      onKeyDown={(ev) => changeDirection(ev as unknown as KeyboardEvent)}
+      className={styles["game-board"]}
+    >
+      <img
+        id="fruit"
+        className={styles["fruit"]}
+        src={AppleLogo}
+        alt="fruit"
+        width="30"
+      />
+      <canvas
+        className={styles["playArea"]}
+        ref={canvasRef}
+        width={`${canvasX}px`}
+        height={`${canvasY}px`}
+      />
+      {gameOver && <div className={styles["gameOver"]}>Game Over</div>}
+      {/* <button onClick={play} className={styles["playButton"]}>
+        Play
+      </button> */}
+      <div className={styles["scoreBox"]}>
+        <h2>Score: {score}</h2>
+        <h2>High Score: {localStorage.getItem("snakeScore")}</h2>
+      </div>
     </div>
   );
-}
+};
 
 export default GameBoard;
