655cadab7ce79d3dc9fdc27950fc3f7168c389c2
CarlosT25-png <cartorresva@gmail.com>
2023-06-18 19:13:30 -0600

    text sign updated

--- 3d-portafolio/src/components/timeMachineScene/world/garage/TextSign.tsx
+++ 3d-portafolio/src/components/timeMachineScene/world/garage/TextSign.tsx
@@ -1,25 +1,20 @@
 import { Text3D } from "@react-three/drei";
+import { useControls } from "leva";
+import { useEffect } from "react";
 
 const TextSign = () => {
   return (
     <>
       <Text3D
         font={"/fonts/future.json"}
-        scale={0.3}
-        position={[2.8, 1.25, -4.75]}
+        scale={0.25}
+        position={[1.585, 1.093, -3.546]}
         rotation-y={-0.7}
+        lineHeight={1}
+        bevelThickness={1000}
       >
-        CARLOS'S
-        <meshBasicMaterial color={"#f2d8ff"} />
-      </Text3D>
-      <Text3D
-        font={"/fonts/future.json"}
-        scale={0.3}
-        position={[3, 0.85, -4.5]}
-        rotation-y={-0.7}
-      >
-        ATTIC
-        <meshBasicMaterial color={"#f2d8ff"} />
+        CARLOS'S ATTIC
+        <meshBasicMaterial color={"#0c24fb"} />
       </Text3D>
     </>
   );
